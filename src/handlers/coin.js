32429ae7f5fdd3e15502baf8e4bccd77b6c3ed99
hbk671104 <hbk671104@gmail.com>
2018-04-25 15:04:37 +0800

    simple formatting

--- src/handlers/coin.js
+++ src/handlers/coin.js
@@ -12,11 +12,11 @@ const requestTotalMarketCap = message => {
 					bitcoin_percentage_of_market_cap
 				} = JSON.parse(res.body)
 				message.say(
-					`总市值: ${accounting.formatMoney(
+					`总市值：${accounting.formatMoney(
 						total_market_cap_usd
-					)}\n24小时总交易量: ${accounting.formatMoney(
+					)}\n24小时总交易量：${accounting.formatMoney(
 						total_24h_volume_usd
-					)}\nBTC占比(%): ${bitcoin_percentage_of_market_cap}`
+					)}\nBTC占比：${bitcoin_percentage_of_market_cap}%`
 				)
 			}
 		}
@@ -34,7 +34,7 @@ const request100Token = message => {
 					c => c.id === content || c.symbol.toLowerCase() === content
 				)
 				if (crypto) {
-					const {
+					let {
 						symbol,
 						rank,
 						price_usd,
@@ -43,13 +43,25 @@ const request100Token = message => {
 						percent_change_24h,
 						percent_change_7d
 					} = crypto
+
+					// percentage formatting
+					percent_change_1h = `${
+						/-/.test(percent_change_1h) ? '↓' : '↑'
+					}${percent_change_1h}%`
+					percent_change_24h = `${
+						/-/.test(percent_change_24h) ? '↓' : '↑'
+					}${percent_change_24h}%`
+					percent_change_7d = `${
+						/-/.test(percent_change_7d) ? '↓' : '↑'
+					}${percent_change_7d}%`
+
 					message.say(
-						`币种: ${symbol}\n市值排名: ${rank}\n现价: ${accounting.formatMoney(
+						`币种：${symbol}\n市值排名：${rank}\n现价：${accounting.formatMoney(
 							price_usd
 						)}/${accounting.formatMoney(
 							price_cny,
 							'￥'
-						)}\n涨跌幅(%): ${percent_change_1h}(1h), ${percent_change_24h}(24h), ${percent_change_7d}(7d)`
+						)}\n涨跌幅：\n${percent_change_1h}（1小时）\n${percent_change_24h}（1天）\n${percent_change_7d}（7天）`
 					)
 				}
 			}
@@ -58,10 +70,10 @@ const request100Token = message => {
 }
 
 const handleCoinMsg = message => {
-	const content = message.content()
+	const content = message.content().trim()
 
 	// total marketcap
-	if (/市值|marketcap/.test(content)) {
+	if (content === '市值' || content === '总市值' || content === 'marketcap') {
 		requestTotalMarketCap(message)
 	}
 
