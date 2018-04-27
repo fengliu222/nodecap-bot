cba966e6cb2c21c83080b2574e5906423433f7ca
hbk671104 <hbk671104@gmail.com>
2018-04-27 15:53:06 +0800

    home_timeline

--- src/handlers/twitter.js
+++ src/handlers/twitter.js
@@ -2,24 +2,22 @@ const Twit = require('twit')
 const { Wechaty } = require('wechaty')
 
 const T = new Twit({
-    consumer_key: 'DO95t59LcGDrAg1PEYpk0mwAd',
-    consumer_secret: '27pPyOPy8EOr16S8g0U9ZcpsgAyKwcC89MdyIwwGfa7FWaiO0X',
-    access_token: '358455402-yg7eh48RQFoG4XHv1jeHRMbfWzIeirDC6rkRYXsd',
-    access_token_secret: 'm9nGT4u12CnAPrh1kwdgHQLMdoo6ZQ2sQcX05veHRDBBY',
-    timeout_ms: 60 * 1000
+	consumer_key: 'K1nI8cNIdZ8Ro3U8KftaFLLGe',
+	consumer_secret: '3gxxFeOizVpkMQlD2hmbkXE8e8N0rXYTQaF3tPJvbsCrlMb2zq',
+	access_token: '989762565457641472-YO55h3gUFFggwHaTRrD5zCxnTr0OeXF',
+	access_token_secret: 'LWPoGo8OC8D5mTQLtowHpxcFVzCTumBoz7pUgndx8zwNQ',
+	timeout_ms: 60 * 1000
 })
 
 const handleTwitterRequest = () => {
-    T.get('statuses/user_timeline', { screen_name: 'IOStoken' }, (err, data, res) => {
-        if (!err) {
-            // const tweets = JSON.parse(data)
-            const tweetTexts = data.map(t => t.text).join('\n\n')
-            // console.log(tweetTexts)
-            Wechaty.instance().say(tweetTexts)
-        }
-    })
+	T.get('statuses/home_timeline', { count: 20 }, (err, data, res) => {
+		if (!err) {
+			const text = data.map(t => `${t.user.name}:\n${t.text}`).join('\n\n')
+			Wechaty.instance().say(text)
+		}
+	})
 }
 
 module.exports = {
-    handleTwitterRequest
+	handleTwitterRequest
 }
