9afc4786cd88b5dd90db74145a09e563191f9b92
hbk671104 <hbk671104@gmail.com>
2018-05-02 17:26:27 +0800

    reformat project structure

--- src/handler/report.js
+++ src/handler/report.js
@@ -5,60 +5,70 @@ const Raven = require('raven')
 
 const { getProjectNews } = require('./news')
 const { getTokenInfo } = require('./coin')
-const { delay } = require('../helper/common')
+const { delay, formatPercentage } = require('../helper/common')
 
 moment.locale('zh-cn')
 
+const requestPeport = async p => {
+	const project = p
+	try {
+		const news = await getProjectNews(p)
+		if (news) {
+			project['news'] = news.content
+			const tokenInfo = await getTokenInfo(p)
+			if (tokenInfo) {
+				project['price_usd'] = tokenInfo.price_usd
+				project['price_cny'] = tokenInfo.price_cny
+				project['rank'] = tokenInfo.rank
+				project['percent_change_1h'] = tokenInfo.percent_change_1h
+				project['percent_change_24h'] = tokenInfo.percent_change_24h
+				project['percent_change_7d'] = tokenInfo.percent_change_7d
+			}
+		}
+	} catch (e) {
+		console.error(e)
+		// Raven.captureException(e)
+	}
+	return project
+}
+
+const createReport = r => {
+	const title = `${r.name}${(r.token && ` (${r.token}) `) || ''}：\n\n`
+	const news = `${(r.news && `动态：${r.news}\n`) || ''}`
+	const price = `${(r.price_usd &&
+		r.price_cny &&
+		`现价：${accounting.formatMoney(r.price_usd)}/${accounting.formatMoney(
+			r.price_cny,
+			'￥'
+		)}\n`) ||
+		''}`
+	const percentage_change = `${(r.percent_change_1h &&
+		r.percent_change_24h &&
+		r.percent_change_7d &&
+		`涨跌幅：${formatPercentage(
+			r.percent_change_1h
+		)} (1小时), ${formatPercentage(
+			r.percent_change_24h
+		)} (24小时), ${formatPercentage(r.percent_change_7d)} (7天)\n`) ||
+		''}`
+
+	return `${title}${news}${price}${percentage_change}`
+}
+
 const generateReport = async () => {
-	const room = await Room.find({ topic: '节点-产品技术Mafia' })
-	if (!room) return
+	// const room = await Room.find({ topic: '节点-产品技术Mafia' })
+	// if (!room) return
 
 	// get project list
 	const projects = require('../data/projects.json')
 
 	// iterate
-	const report_raw = await Promise.all(
-		projects.map(async p => {
-			const project = p
-			try {
-				const news = await getProjectNews(p)
-				if (news) {
-					project['news'] = news.content
-				}
-			} catch (e) {
-				console.error(e)
-				// Raven.captureException(e)
-			}
-			try {
-				const tokenInfo = await getTokenInfo(p)
-				if (tokenInfo) {
-					project['price_usd'] = tokenInfo.price_usd
-					project['price_cny'] = tokenInfo.price_cny
-				}
-			} catch (e) {
-				console.error(e)
-			}
-			return project
-		})
-	)
+	const report_raw = await Promise.all(projects.map(requestPeport))
 	let report_text = report_raw
-		.filter(r => r.news || r.price_cny || r.price_usd)
-		.map(
-			r =>
-				`${r.name}${r.token ? `（${r.token}）` : ''}：\n${
-					r.news ? `动态：${r.news}\n` : ''
-				}${
-					r.price_usd || r.price_cny
-						? `价格：${accounting.formatMoney(
-								r.price_usd
-						  )}/${accounting.formatMoney(r.price_cny, '￥')}`
-						: ''
-				}`
-		)
-		.join('\n\n')
-	report_text = `节点投资项目动态汇总(${moment().format(
-		'L'
-	)})：\n\n${report_text}`
+		.filter(r => r.news)
+		.map(createReport)
+		.join('\n')
+	report_text = `节点投后跟踪汇总(${moment().format('L')})：\n\n${report_text}`
 
 	// say it
 	// room.say(report_text)
