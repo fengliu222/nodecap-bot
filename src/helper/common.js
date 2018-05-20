f744224995bc8b3a3e9da0e57f0bcbfbec07e819
hbk671104 <hbk671104@gmail.com>
2018-05-20 21:13:55 +0800

    bug fixes

--- src/helper/common.js
+++ src/helper/common.js
@@ -1,6 +1,10 @@
 const delay = ms => new Promise(res => setTimeout(res, ms))
-const formatPercentage = p =>
-	/-/.test(p) ? `${p.replace('-', '↓ ')}%` : `↑ ${p}%`
+const formatPercentage = p => {
+	if (p) {
+		return /-/.test(p) ? `${p.replace('-', '↓ ')}%` : `↑ ${p}%`
+	}
+	return null
+}
 
 module.exports = {
 	delay,
