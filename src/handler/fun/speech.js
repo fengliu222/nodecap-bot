f0986358dbefc4bad2a3fa17bda5b36232d6a674
hbk671104 <hbk671104@gmail.com>
2018-07-01 18:36:38 +0800

    blah

--- src/handler/fun/speech.js
+++ src/handler/fun/speech.js
@@ -1,28 +1,37 @@
-const moment = require('moment-business-time')
+const moment = require('moment')
+const business = require('moment-business')
 
 const whoGivesSpeechTmr = ({ content }) => {
 	if (!/下次谁分享/.test(content)) {
 		return
 	}
 
-	const team = ['剑锋', '明远', '博康', '朋哥', '禹涛', 'Maggie', '振阔']
+	const team = [
+		'Maggie',
+		'明远',
+		'剑锋',
+		'博康',
+		'朋哥',
+		'禹涛',
+		'振阔',
+		'苏菲',
+	]
 	const now = moment()
-	const start = moment('06/19/2018', 'MM/DD/YYYY')
+	const start = moment('07/02/2018', 'MM/DD/YYYY')
 	if (now.isBefore(start)) {
 		return team[0]
 	}
-	let diff = now.workingDiff(start, 'days') + 1
+	// let diff = now.workingDiff(start, 'days') + 1
+	const diff = business.weekDays(start, now)
 	const modulo = () => {
 		const modulo = diff % team.length
 		if (modulo === team.length) {
 			return 0
 		}
-		return modulo
+		return modulo + 1
 	}
 
-	let mod = modulo()
-
-	return team[mod]
+	return team[modulo()]
 }
 
 module.exports = {
