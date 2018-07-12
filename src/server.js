26b29e233a6cdb2262815275c5763fe17c795438
hbk671104 <hbk671104@gmail.com>
2018-07-12 13:30:38 +0800

    other fixes

--- src/server.js
+++ src/server.js
@@ -47,11 +47,9 @@ const bot = new Wechaty()
 bot.on('scan', (qrcode, status) => {
 	qrTerm.generate(qrcode, { small: true }) // show qrcode on console
 
-	const qrcodeImageUrl = [
-		'https://api.qrserver.com/v1/create-qr-code/?data=',
-		encodeURIComponent(qrcode),
-		'&size=220x220&margin=20',
-	].join('')
+	const qrcodeImageUrl = `https://api.qrserver.com/v1/create-qr-code/?data=
+		${encodeURIComponent(qrcode)}
+		'&size=220x220&margin=20`
 
 	console.log(qrcodeImageUrl)
 })
