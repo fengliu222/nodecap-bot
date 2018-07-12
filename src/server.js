aa78a7603fa8603941f1839d5a07b51b396c4a3c
hbk671104 <hbk671104@gmail.com>
2018-07-12 14:05:56 +0800

    add room join logic

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const Schedule = require('node-schedule')
 const Raven = require('raven')
 const qrTerm = require('qrcode-terminal')
-const { Wechaty, Room } = require('wechaty')
+const { Wechaty, MediaMessage, Room } = require('wechaty')
 
 const { bot: replyBot } = require('./bot')
 const { mail } = require('./handler/mail')
@@ -29,35 +29,35 @@ Schedule.scheduleJob('0 0 * * *', async () => {
 
 const getRandomArbitrary = (min, max) => Math.random() * (max - min) + min
 
-// app.get('/api/nodus-bot', async (req, res) => {
-// 	const params = req.query
-// 	const data = await bot(params)
-
-// 	setTimeout(() => {
-// 		res.json({
-// 			data: data || 'empty_message',
-// 		})
-// 	}, getRandomArbitrary(0, 3000))
-// })
-
-// app.listen(9001)
-
 const bot = new Wechaty()
 
 bot.on('scan', (qrcode, status) => {
 	qrTerm.generate(qrcode, { small: true }) // show qrcode on console
 
-	const qrcodeImageUrl = `https://api.qrserver.com/v1/create-qr-code/?data=
-		${encodeURIComponent(qrcode)}
-		'&size=220x220&margin=20`
+	const qrcodeImageUrl = `https://api.qrserver.com/v1/create-qr-code/?data=${encodeURIComponent(
+		qrcode,
+	)}&size=220x220&margin=20`
 
 	console.log(qrcodeImageUrl)
 })
 bot.on('login', user => {
 	console.log(`${user} login`)
 })
-bot.on('logout', user => {
-	console.log(`${user} logout`)
+bot.on('room-join', async (room, inviteeList, inviter) => {
+	const topic = await room.topic()
+	if (/Hotnode x|x Hotnode/.test(topic)) {
+		const intro =
+			'老板好🙇，hotnode是源自Token Fund日常工作需要而衍生的一款企业资管工具，包含了基金收益率实时统计、项目募投管退管理、权限设置、人脉管理等功能。目前产品已经完成了4.0版本，还处于不断迭代之中，还希望老板在使用过程中多给我们提提意见，帮助Hodenode更快成长，更加契合老板在工作中的需求。\n\n此群是咱们Fund的专属群，群内包含了Hotnode的产品经理、工程师、客户经理，能够7*24随时在线，回答老板在使用过程中可能遇到的各类问题。😄\n\n下图是Hotnode的产品使用手册，里面包含了项目的研发背景、首次启动步骤、各项功能的具体使用方式，望查看。'
+		const manual = new MediaMessage('./data/Hotnode产品手册v1.pdf')
+		const download =
+			'1、Web端访问地址：http://www.hotnode.io\n2、iOS下载：https://fir.im/hotnode\n3、Android下载：https://fir.im/hotnodeAndroid'
+
+		await room.say(intro)
+		await room.say(manual)
+		await room.say(download)
+	}
+
+	console.log(`Room ${topic}, invited by ${inviter}`)
 })
 bot.on('message', async msg => {
 	if (msg.self()) return
@@ -76,11 +76,13 @@ bot.on('message', async msg => {
 
 	const response = await replyBot({ name, content })
 
-	setTimeout(() => {
-		msg.say(response)
+	setTimeout(async () => {
+		await msg.say(response)
+		console.log(name, content)
 	}, getRandomArbitrary(0, 3000))
-
-	console.log(name, content)
+})
+bot.on('logout', user => {
+	console.log(`${user} logout`)
 })
 
 bot.start().catch(console.error)
